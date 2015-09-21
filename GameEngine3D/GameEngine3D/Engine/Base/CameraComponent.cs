e7c6281e565b3d6048330410db4f7e4c9fc3c6e0
Reuben Cummins <reubencummins@gmail.com>
2015-09-21 10:24:28 +0100

    Added camera, game utilities and input engine

---
+++ GameEngine3D/GameEngine3D/Engine/Base/CameraComponent.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEngine3D.Engine.Base
+{
+    class CameraComponent : Component
+    {
+        //rotates world by camera transforms
+        public Matrix View { get; set; }
+        //connects 3D points to 2D screen space coordinates
+        public Matrix Projection { get; set; }
+        //how close to the camera will things be rendered
+        public float NearPlane { get; set; }
+        //how far from the camera will things be rendered
+        public float FarPlane { get; set; }
+
+        //what is the camera looking at
+        public Vector3 Target { get; set; }
+        //which way is up
+        public Vector3 UpDirection { get; set; }
+        
+        public Vector3 CameraDirection { get; set; }
+
+        public CameraComponent(string id,Vector3 target)
+        {
+            ID = id;
+            Target = target;
+            UpDirection = new Vector3(0, 1, 0);
+        }
+
+        public void Initialise()
+        {
+            NearPlane = 1;
+            FarPlane = 1000;
+            UpdateViewMatrix();
+            //take out FOV to ini
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(80), GameUtilities.GraphicsDevice.PresentationParameters.BackBufferWidth/GameUtilities.GraphicsDevice.PresentationParameters.BackBufferHeight, NearPlane, FarPlane);
+            base.Initalise();
+        }
+
+        public virtual void UpdateViewMatrix()
+        {
+
+            CameraDirection = Manager.Owner.Location - Target;
+
+            View = Matrix.CreateLookAt(Manager.Owner.Location, Target, UpDirection);
+        }
+    }
+}
---
+++ GameEngine3D/GameEngine3D/Engine/GameUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace GameEngine3D.Engine
+{
+    public static class GameUtilities
+    {
+        public static GraphicsDevice GraphicsDevice { get; set; }
+        public static GameTime Time { get; set; }
+        public static float Delta { get; set; }
+        public static int MyProperty { get { return Time.ElapsedGameTime.Milliseconds; } }
+        public static Random Random { get; set; }
+        public static SpriteFont DebugFont { get; set; }
+        public static SpriteBatch DebugBatch { get; set; }
+
+        public static ContentManager PersistentContent { get; set; }
+        public static ContentManager NonPersistentContent { get; set; }
+
+        public static void UnloadNonPersistentContent()
+        {
+            if (NonPersistentContent != null)
+            {
+                NonPersistentContent = null;
+            }
+        }
+    }
+}
